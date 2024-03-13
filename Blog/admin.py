310a02326ed66b9940e777b18b08dca2f8f68327
= <=>
2024-03-13 10:13:36 +0330

    edit models and registered it in admin panel with filters and search bars

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post ,Category , Comment
 
-# Register your models here.
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'parent']
+    search_fields = ['name']
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'created_at' ,'category', 'intro']
+    list_filter = ['author','created_at' , 'category']
+    search_fields = ['title']
+    ordering = ['created_at']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.author:
+            obj.author = request.user
+        obj.save()
+
+class CommentModelAdmin(admin.ModelAdmin):
+    list_filter = ['post' , 'created_at' , ]
+    ordering = ['created_at']
+
+admin.site.register(Post ,PostModelAdmin )
+admin.site.register(Category , CategoryModelAdmin)
+admin.site.register(Comment ,CommentModelAdmin )
--- Blog/models.py
+++ Blog/models.py
@@ -11,29 +11,30 @@ class Category(models.Model):
         verbose_name = "Caregories"
         
     def __str__(self):
-        return self.name
-    
+        if self.parent :
+            return f"{self.name}   -> {self.parent.name}"
+        else :
+             return f'{self.name}'
 
 
 class Comment(models.Model):
-    user =models.ForeignKey(User , on_delete=models.CASCADE , null=False , blank=False)
+    name = models.CharField(max_length=50 , null=True , blank=True)
+    #user =models.ForeignKey(User , on_delete=models.CASCADE , null=False , blank=False)
     email = models.EmailField(null=True , blank=True) 
     content = models.CharField(max_length=300)
     post = models.ForeignKey('Post' , on_delete=models.CASCADE , null=False , blank=False , related_name='comments')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name = "Caregories"
+        verbose_name = "Comments"
         ordering = ['id','created_at']
 
     def __str__(self):
         return f"id:{self.id} | post:{self.post.title} | {self.content}"
 
 class Post(models.Model):
-    def get_default_user():
-        return User.objects.get(username='anonymous')
 
-    author = models.ForeignKey(User , on_delete=models.SET_DEFAULT , default=get_default_user)
+    author = models.ForeignKey(User , on_delete=models.SET_NULL , null=True , blank=True ,related_name="posts")
     title = models.CharField(max_length=100)
     intro = models.CharField( max_length=200)
     image = models.ImageField()
